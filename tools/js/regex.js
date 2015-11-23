5bc846dede6a209229b1ac2f752ed059b60a6f51
galunni <galunni@users.noreply.github.com>
2015-11-23 15:23:34 +0100

    Update regex.js

--- tools/js/regex.js
+++ tools/js/regex.js
@@ -1,1 +1,57 @@
+// giovanni 2015
 
+var testo = "";
+    
+function adoptText()
+{
+    testo =  
+    document.getElementById('textOutput').innerHTML = 
+    document.getElementById('textInput').value;
+    
+    updateResults();
+}
+    
+function updateResults()
+{
+  if(document.forms.formulario.testo_sub_1.value.length >= 1) {
+    substituteIt(); 
+    }
+  else if(document.forms.formulario.testo_match.value.length >= 1) {
+    matchIt(); 
+    }
+}
+    
+function getFlags()
+{
+   var flags = "";  // reading modifiers
+    
+   flags += (document.forms.formulario.global_flag.checked) ? "g" : ""; // global 
+   flags += (document.forms.formulario.ignore_flag.checked) ? "i" : ""; // ignore case
+   flags += (document.forms.formulario.multi_flag.checked)  ? "m" : ""; // multiline
+    
+  return flags;
+}
+function matchIt()
+{
+   var rr = 
+   eval("/" + document.forms.formulario.testo_match.value + "/" + getFlags());
+       
+  document.getElementById("textOutput").innerHTML = 
+  testo.replace(rr, '<b class="match">' + "$&" + '</b>');
+    
+  // update bg color of selected method (match)  
+  document.getElementById("matcher").style.backgroundColor="#8C9C88";
+  document.getElementById("subster").style.backgroundColor="transparent";
+}
+    
+function substituteIt()
+{
+   var r1 = eval("/" + document.forms.formulario.testo_sub_1.value + "/" + getFlags());
+   var r2 = document.forms.formulario.testo_sub_2.value;
+  document.getElementById("textOutput").innerHTML = 
+    testo.replace(r1, '<b class="subst">' + r2  + '</b>');
+    
+  // update bg color of selected method (substitute)
+  document.getElementById("subster").style.backgroundColor="#F2DBAE";
+  document.getElementById("matcher").style.backgroundColor="transparent";
+}
