ed8fa9e7ba4c787bd8973fc97b03363891cf8564
galunni <galunni@users.noreply.github.com>
2020-02-23 21:06:04 +0100

    updating indenting style

--- tools/js/regex.js
+++ tools/js/regex.js
@@ -1,24 +1,24 @@
-// regex.js
+/* regex.js
 // giovanni 2015
-// making use of the javascript regex engine to test a regular expression on a given text input
+// making use of the javascript regex engine
+// to test a regular expression on a given text input
 
 // beta version installed on:
 // http://galunni.github.io/tools/regex_tester.html
+*/
 
 var testo = "";
 var activeFunction = "match";
     
-function adoptText()
-{
-    testo =  
-    document.getElementById('textOutput').innerHTML = 
-    document.getElementById('textInput').value;
+function adoptText(){
+  testo =  
+  document.getElementById('textOutput').innerHTML = 
+  document.getElementById('textInput').value;
     
-    updateResults();
+  updateResults();
 }
     
-function updateResults()
-{
+function updateResults(){
   if(activeFunction === "match"){
     matchIt(); 
   }
@@ -27,29 +27,27 @@ function updateResults()
   }
   else if(document.forms.formulario.testo_sub_1.value.length >= 1) {
     substituteIt(); 
-    }
+  }
   else if(document.forms.formulario.testo_match.value.length >= 1) {
     matchIt(); 
-    }
+  }
 }
     
-function getFlags()
-{
-   var flags = "";  // reading modifiers
+function getFlags(){
+  var flags = "";  // reading modifiers
     
-   flags += (document.forms.formulario.global_flag.checked) ? "g" : ""; // global 
-   flags += (document.forms.formulario.ignore_flag.checked) ? "i" : ""; // ignore case
-   flags += (document.forms.formulario.multi_flag.checked)  ? "m" : ""; // multiline
+  flags += (document.forms.formulario.global_flag.checked) ? "g" : ""; // global 
+  flags += (document.forms.formulario.ignore_flag.checked) ? "i" : ""; // ignore case
+  flags += (document.forms.formulario.multi_flag.checked)  ? "m" : ""; // multiline
     
   return flags;
 }
 
-function matchIt()
-{
-   activeFunction = "match";
+function matchIt(){
+  activeFunction = "match";
    
-   var rr = 
-   eval("/" + document.forms.formulario.testo_match.value + "/" + getFlags());
+  var rr = 
+  eval("/" + document.forms.formulario.testo_match.value + "/" + getFlags());
        
   document.getElementById("textOutput").innerHTML = 
   testo.replace(rr, '<b class="match">' + "$&" + '</b>');
@@ -59,12 +57,11 @@ function matchIt()
   document.getElementById("subster").style.backgroundColor="transparent";
 }
     
-function substituteIt()
-{
+function substituteIt(){
    activeFunction = "subst";
    
-   var r1 = eval("/" + document.forms.formulario.testo_sub_1.value + "/" + getFlags());
-   var r2 = document.forms.formulario.testo_sub_2.value;
+  var r1 = eval("/" + document.forms.formulario.testo_sub_1.value + "/" + getFlags());
+  var r2 = document.forms.formulario.testo_sub_2.value;
   document.getElementById("textOutput").innerHTML = 
     testo.replace(r1, '<b class="subst">' + r2  + '</b>');
     
